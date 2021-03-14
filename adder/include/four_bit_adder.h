d5dd2b691a3a99e25ce04c43532c7ecd8e8a5371
Nawab Ali <nawabali@MacBook-Pro.domain>
2021-03-13 17:13:26 -0800

    Refactor code.

--- adder/include/four_bit_adder.h
+++ adder/include/four_bit_adder.h
@@ -17,44 +17,42 @@ SC_MODULE (four_bit_adder) {
     sc_out<bool> sum[4];
 
     full_adder fa0, fa1, fa2, fa3;
-    sc_signal<bool> c1, c2, c3;
-    sc_signal<bool> s0, s1, s2, s3;
+    sc_signal<bool> c[3], s[4];
 
     SC_CTOR (four_bit_adder) : fa0("full_adder_0"), fa1("full_adder_1"), fa2("full_adder_2"), fa3("full_adder_3") {
         SC_METHOD(generate_sum);
         dont_initialize();
-        sensitive << s0 << s1 << s2 << s3;
+        sensitive << s[0] << s[1] << s[2] << s[3];
 
         fa0.a(a[0]);
         fa0.b(b[0]);
         fa0.c_in(c_in);
-        fa0.sum(s0);
-        fa0.c_out(c1);
+        fa0.sum(s[0]);
+        fa0.c_out(c[0]);
 
         fa1.a(a[1]);
         fa1.b(b[1]);
-        fa1.c_in(c1);
-        fa1.sum(s1);
-        fa1.c_out(c2);
+        fa1.c_in(c[0]);
+        fa1.sum(s[1]);
+        fa1.c_out(c[1]);
 
         fa2.a(a[2]);
         fa2.b(b[2]);
-        fa2.c_in(c2);
-        fa2.sum(s2);
-        fa2.c_out(c3);
+        fa2.c_in(c[1]);
+        fa2.sum(s[2]);
+        fa2.c_out(c[2]);
 
         fa3.a(a[3]);
         fa3.b(b[3]);
-        fa3.c_in(c3);
-        fa3.sum(s3);
+        fa3.c_in(c[2]);
+        fa3.sum(s[3]);
         fa3.c_out(c_out);
     }
 
     void generate_sum() {
-        sum[0].write(s0);
-        sum[1].write(s1);
-        sum[2].write(s2);
-        sum[3].write(s3);
+        for (int i = 0; i < 4; ++i) {
+            sum[i].write(s[i]);
+        }
     }
 };
 
