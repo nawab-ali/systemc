6744d5d0b3a021e7bd5a645009286e74833c9675
Nawab Ali <nawabali@Nawabs-MacBook-Pro.local>
2021-04-01 07:54:23 -0700

    Implement 2x4 decoder.

--- encoder_decoder/include/two_four_decoder_tb.h
+++ encoder_decoder/include/two_four_decoder_tb.h
@@ -9,5 +9,34 @@
 
 #include <systemc.h>
 
+SC_MODULE (two_four_decoder_tb) {
+    sc_in<bool> clk;
+    sc_out<bool> enable;
+    sc_out<sc_uint<2>> input;
+
+    SC_CTOR (two_four_decoder_tb) {
+        SC_THREAD(gen_stimuli);
+        dont_initialize();
+        sensitive << clk.pos();
+    }
+
+    void gen_stimuli() {
+        wait();
+
+        enable.write(1);
+        for (int i = 0; i < 4; ++i) {
+            input.write(i);
+            wait();
+        }
+
+        enable.write(0);
+        for (int i = 0; i < 4; ++i) {
+            input.write(i);
+            wait();
+        }
+
+        sc_stop();
+    }
+};
 
 #endif //TWO_FOUR_DECODER_TB_H
--- encoder_decoder/src/two_four_decoder_sim.cpp
+++ encoder_decoder/src/two_four_decoder_sim.cpp
@@ -5,8 +5,41 @@
  */
 
 #include <systemc.h>
+#include "two_four_decoder.h"
+#include "two_four_decoder_tb.h"
+
+sc_trace_file* create_vcd_trace(const char* file, sc_signal<bool>& enable, sc_signal<sc_uint<2>>& input,
+                                sc_signal<sc_uint<4>>& output) {
+    sc_trace_file* fp = sc_create_vcd_trace_file(file);
+
+    sc_trace(fp, enable, "enable");
+    sc_trace(fp, input, "input");
+    sc_trace(fp, output, "output");
+
+    return fp;
+}
 
 void simulate_two_four_decoder() {
+    sc_trace_file* fp;
+    sc_signal<bool> enable;
+    sc_signal<sc_uint<2>> input;
+    sc_signal<sc_uint<4>> output;
+    sc_clock clk("clk", 10, SC_NS, 0.5);
+
+    two_four_decoder decoder_2x4("2x4_decoder");
+    two_four_decoder_tb decoder_2x4_tb("2x4_decoder_testbench");
+
+    decoder_2x4.enable(enable);
+    decoder_2x4.input(input);
+    decoder_2x4.output(output);
+
+    decoder_2x4_tb.clk(clk);
+    decoder_2x4_tb.enable(enable);
+    decoder_2x4_tb.input(input);
+
+    fp = create_vcd_trace("two_four_decoder", enable, input, output);
+    sc_start(300, SC_NS);
+    sc_close_vcd_trace_file(fp);
 }
 
 int sc_main(int argc, char** argv) {
