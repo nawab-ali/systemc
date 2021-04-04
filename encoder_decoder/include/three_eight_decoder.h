c8ac847fe4cb2ce2f9ea5bb846544c216036b86b
Nawab Ali <nawabali@Nawabs-MacBook-Pro.local>
2021-04-04 11:23:12 -0700

    Implement 3x8 decoder.

--- encoder_decoder/include/three_eight_decoder.h
+++ encoder_decoder/include/three_eight_decoder.h
@@ -15,14 +15,47 @@ SC_MODULE (three_eight_decoder) {
     sc_in<sc_uint<3>> input;
     sc_out<sc_uint<8>> output;
 
-    SC_CTOR (three_eight_decoder) {
-        SC_METHOD(decode);
+    two_four_decoder decoder_2x4_1, decoder_2x4_2;
+    sc_signal<bool> e1, e2;
+    sc_signal<sc_uint<2>> i;
+    sc_signal<sc_uint<4>> o1, o2;
+
+    SC_CTOR (three_eight_decoder) : decoder_2x4_1("decoder_2x4_1"), decoder_2x4_2("decoder_2x4_2") {
+        SC_METHOD(process_input);
         dont_initialize();
-        sensitive << input << enable;
+        sensitive << input;
+
+        SC_METHOD(process_output);
+        dont_initialize();
+        sensitive << o1 << o2;
+
+        if (enable.read()) {
+            decoder_2x4_1.enable(e1);
+            decoder_2x4_1.input(i);
+            decoder_2x4_1.output(o1);
+
+            decoder_2x4_2.enable(e2);
+            decoder_2x4_2.input(i);
+            decoder_2x4_2.output(o2);
+        } else {
+            output.write(0);
+        }
     }
 
-    void decode() {
+    void process_input() {
+        sc_uint<3> temp = input.read();
 
+        e1.write(temp[2]);
+        e2.write(~temp[2]);
+        i.write(temp.range(0, 1));
+    }
+
+    void process_output() {
+        sc_uint<8> temp;
+
+        temp.range(0, 3) = o1;
+        temp.range(4, 7) = o2;
+        output.write(temp);
     }
 };
 
