095f17eab96fd336509aafded6ff6ed3c27b1414
Nawab Ali <nawabali@Nawabs-MacBook-Pro.local>
2021-05-01 06:27:48 -0700

    Initial commit for CPU/Memory TLM transactions using DMI.

---
+++ tlm/cpu_mem_dmi/include/top.h
@@ -0,0 +1,31 @@
+/**
+ * @file top.h
+ * @brief This file connects the TLM initiator and target.
+ * @author Nawab Ali
+ */
+
+#ifndef TOP_H
+#define TOP_H
+
+#include "cpu.h"
+#include "memory.h"
+#include <systemc.h>
+
+SC_MODULE (Top) {
+    Cpu *cpu;
+    Memory *memory;
+
+    SC_CTOR (Top) {
+        cpu = new Cpu("cpu");
+        memory = new Memory("memory");
+
+        cpu->socket.bind(memory->socket);
+    }
+
+    ~Top() {
+        delete cpu;
+        delete memory;
+    }
+};
+
+#endif //TOP_H
