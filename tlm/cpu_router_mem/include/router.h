37fc6e88d6dc0d9bedf7a9c1786c0fa98ef435d8
Nawab Ali <nawabali@Nawabs-MacBook-Pro.local>
2021-05-15 07:35:03 -0700

    Implement constructor/destructor for Router.

--- tlm/cpu_router_mem/include/router.h
+++ tlm/cpu_router_mem/include/router.h
@@ -9,32 +9,37 @@
 
 #include <tlm.h>
 #include <systemc.h>
+#include <tlm_utils/simple_target_socket.h>
 #include <tlm_utils/simple_initiator_socket.h>
 
-using namespace std;
+const unsigned int size = 16;
+const unsigned int num_targets = 4;
 
+template <unsigned int num_targets>
 SC_MODULE (Router) {
-
-
     // TLM-2 socket, defaults to 32-bits wide, base protocol
-    tlm_utils::simple_initiator_socket<Cpu> socket;
+    tlm_utils::simple_target_socket<Router> target_socket;
+    tlm_utils::simple_initiator_socket_tagged<Router>* initiator_socket[num_targets];
 
-    SC_CTOR (Router) : socket("socket") {
+    SC_CTOR (Router) : target_socket("target_socket") {
         // Register callbacks for incoming interface method calls
-        socket.register_invalidate_direct_mem_ptr(this, &Cpu::invalidate_direct_mem_ptr);
+        target_socket.register_b_transport(this, &Router::b_transport);
+        target_socket.register_get_direct_mem_ptr(this, &Router::get_direct_mem_ptr);
 
-        trans = new tlm::tlm_generic_payload;
-        SC_THREAD(generate_payload);
+        for (int i = 0; i < num_targets; ++i) {
+            char socket_id[size];
+            snprintf(socket_id, size, "socket_%d", i);
+            initiator_socket[i] = new tlm_utils::simple_initiator_socket_tagged<Router>(socket_id);
+
+            // Register callbacks for incoming interface method calls, including tags
+            initiator_socket[i]->register_invalidate_direct_mem_ptr(this, &Router::invalidate_direct_mem_ptr, i);
+        }
     }
 
-
-    // Use transport interface for communicating with target
-
-
-
-
-
     ~Router() {
+        for (int i = 0; i < num_targets; ++i) {
+            delete initiator_socket[i];
+        }
     }
 };
 
