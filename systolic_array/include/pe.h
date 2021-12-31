ad82b5603c34a834fe474b165ae3481cc91006c6
Nawab Ali <nawabali@Nawabs-MacBook-Pro.local>
2021-12-31 06:41:30 -0800

    Initial commit of skeleton files for systolic array.

---
+++ systolic_array/include/pe.h
@@ -0,0 +1,19 @@
+/**
+ * @file pe.h
+ * @brief This file implements the Processing Element of a Systolic Array.
+ * @author Nawab Ali
+ */
+
+#ifndef PE_H
+#define PE_H
+
+#include <systemc.h>
+
+using namespace std;
+
+SC_MODULE (PE) {
+    SC_CTOR (PE) {
+    }
+};
+
+#endif //PE_H
---
+++ systolic_array/include/systolic_array_tb.h
@@ -0,0 +1,17 @@
+/**
+ * @file systolic_array_tb.h
+ * @brief This file implements the testbench for the Systolic Array.
+ * @author Nawab Ali
+ */
+
+#ifndef SYSTOLIC_ARRAY_TB_H
+#define SYSTOLIC_ARRAY_TB_H
+
+#include <systemc.h>
+
+SC_MODULE (systolic_array_tb) {
+    SC_CTOR (systolic_array_tb) {
+    }
+};
+
+#endif //SYSTOLIC_ARRAY_TB_H
