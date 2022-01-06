e93e3521902fbcd17c195c890d1a0f13be8ea319
Nawab Ali <nawabali@Nawabs-MacBook-Pro.local>
2022-01-05 21:44:33 -0800

    Skeleton implementation of Systolic Array.

---
+++ systolic_array/include/systolic_array.h
@@ -0,0 +1,35 @@
+/**
+ * @file systolic_array.h
+ * @brief This file implements a Weight-stationary Systolic Array.
+ * @author Nawab Ali
+ */
+
+#ifndef SYSTOLIC_ARRAY_H
+#define SYSTOLIC_ARRAY_H
+
+#include "pe.h"
+#include <systemc.h>
+
+template<uint8_t N>
+SC_MODULE (systolic_array) {
+public:
+    sc_in<bool> clk;
+    sc_uint<8> weights[N][N];
+
+    SC_CTOR (systolic_array) {
+        SC_METHOD(matmul);
+        dont_initialize();
+        sensitive << clk.pos();
+    }
+
+    void matmul() {
+
+    }
+
+private:
+    void init_weights() {
+
+    }
+};
+
+#endif //SYSTOLIC_ARRAY_H
