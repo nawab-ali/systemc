6bfe5fbb3b0e9aafb8cf1bca5cc13923d146c8ec
Nawab Ali <nawabali@Nawabs-MacBook-Pro.local>
2022-01-15 07:07:55 -0800

    1. Rename pe_array -> pe_grid 2. Use sc_vector instead of array

--- systolic_array/include/systolic_array.h
+++ systolic_array/include/systolic_array.h
@@ -15,9 +15,9 @@ template<uint8_t N>
 SC_MODULE (systolic_array) {
 public:
     sc_in<bool> clk;
-    sc_in<sc_int<8>> activation_in[N];
-    sc_in<sc_int<32>> partial_sum_in[N];
-    sc_out<sc_int<32>> partial_sum_out[N];
+    sc_vector<sc_in<sc_int<8>>> activation_in{"activation_in", N};
+    sc_vector<sc_in<sc_int<32>>> partial_sum_in{"partial_sum_in", N};
+    sc_vector<sc_out<sc_int<32>>> partial_sum_out{"partial_sum_out", N};
 
     SC_CTOR (systolic_array) {
         SC_METHOD(matmul);
@@ -28,12 +28,13 @@ public:
         init_weights();
         init_act_signals();
         init_psum_signals();
-        create_pe_array();
+        create_pe_grid();
     }
 
 private:
-    pe pe_array[N][N];
+    pe pe_grid[N][N];
     sc_int<8> weights[N][N];
+    sc_out<sc_int<8>> activation_out[N];
 
     // Define signals for PE->PE data transfer
     sc_signal<sc_int<8>> activation_s[N][N-1];
@@ -45,54 +46,54 @@ private:
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < N; ++j) {
                 // Set inputs/outputs for each PE
-                pe_array[i][j].clk(clk);
+                pe_grid[i][j].clk(clk);
 
                 // Corner cases
                 if (i == 0 && j == 0) {
-                    pe_array[i][j].activation_in(activation_in[i]);
-                    pe_array[i][j].partial_sum_in(partial_sum_in[j]);
-                    pe_array[i][j].activation_out(activation_s[i][j]);
-                    pe_array[i][j].partial_sum_out(partial_sum_s[i][j]);
+                    pe_grid[i][j].activation_in(activation_in[i]);
+                    pe_grid[i][j].partial_sum_in(partial_sum_in[j]);
+                    pe_grid[i][j].activation_out(activation_s[i][j]);
+                    pe_grid[i][j].partial_sum_out(partial_sum_s[i][j]);
                 } else if (i == 0 && j == N-1) {
-                    pe_array[i][j].activation_in(activation_s[i][j-1]);
-                    pe_array[i][j].partial_sum_in(partial_sum_in[j]);
-                    pe_array[i][j].activation_out(0);
-                    pe_array[i][j].partial_sum_out(partial_sum_s[i][j]);
+                    pe_grid[i][j].activation_in(activation_s[i][j-1]);
+                    pe_grid[i][j].partial_sum_in(partial_sum_in[j]);
+                    pe_grid[i][j].activation_out(activation_out[i]);
+                    pe_grid[i][j].partial_sum_out(partial_sum_s[i][j]);
                 } else if (i == N-1 && j == 0) {
-                    pe_array[i][j].activation_in(activation_in[i]);
-                    pe_array[i][j].partial_sum_in(partial_sum_s[i-1][j]);
-                    pe_array[i][j].activation_out(activation_s[i][j]);
-                    pe_array[i][j].partial_sum_out(partial_sum_out[j]);
+                    pe_grid[i][j].activation_in(activation_in[i]);
+                    pe_grid[i][j].partial_sum_in(partial_sum_s[i-1][j]);
+                    pe_grid[i][j].activation_out(activation_s[i][j]);
+                    pe_grid[i][j].partial_sum_out(partial_sum_out[j]);
                 } else if (i == N-1 && j == N-1) {
-                    pe_array[i][j].activation_in(activation_s[i][j-1]);
-                    pe_array[i][j].partial_sum_in(partial_sum_s[i-1][j]);
-                    pe_array[i][j].activation_out(0);
-                    pe_array[i][j].partial_sum_out(partial_sum_out[j]);
+                    pe_grid[i][j].activation_in(activation_s[i][j-1]);
+                    pe_grid[i][j].partial_sum_in(partial_sum_s[i-1][j]);
+                    pe_grid[i][j].activation_out(activation_out[i]);
+                    pe_grid[i][j].partial_sum_out(partial_sum_out[j]);
                 } else if (i == 0) {
-                    pe_array[i][j].activation_in(activation_s[i][j-1]);
-                    pe_array[i][j].partial_sum_in(partial_sum_in[j]);
-                    pe_array[i][j].activation_out(activation_s[i][j]);
-                    pe_array[i][j].partial_sum_out(partial_sum_s[i][j]);
+                    pe_grid[i][j].activation_in(activation_s[i][j-1]);
+                    pe_grid[i][j].partial_sum_in(partial_sum_in[j]);
+                    pe_grid[i][j].activation_out(activation_s[i][j]);
+                    pe_grid[i][j].partial_sum_out(partial_sum_s[i][j]);
                 } else if (j == 0) {
-                    pe_array[i][j].activation_in(activation_in[i]);
-                    pe_array[i][j].partial_sum_in(partial_sum_s[i-1][j]);
-                    pe_array[i][j].activation_out(activation_s[i][j]);
-                    pe_array[i][j].partial_sum_out(partial_sum_s[i][j]);
+                    pe_grid[i][j].activation_in(activation_in[i]);
+                    pe_grid[i][j].partial_sum_in(partial_sum_s[i-1][j]);
+                    pe_grid[i][j].activation_out(activation_s[i][j]);
+                    pe_grid[i][j].partial_sum_out(partial_sum_s[i][j]);
                 } else if (i == N-1) {
-                    pe_array[i][j].activation_in(activation_s[i][j-1]);
-                    pe_array[i][j].partial_sum_in(partial_sum_s[i-1][j]);
-                    pe_array[i][j].activation_out(activation_s[i][j]);
-                    pe_array[i][j].partial_sum_out(partial_sum_out[j]);
+                    pe_grid[i][j].activation_in(activation_s[i][j-1]);
+                    pe_grid[i][j].partial_sum_in(partial_sum_s[i-1][j]);
+                    pe_grid[i][j].activation_out(activation_s[i][j]);
+                    pe_grid[i][j].partial_sum_out(partial_sum_out[j]);
                 } else if (j == N-1) {
-                    pe_array[i][j].activation_in(activation_s[i][j-1]);
-                    pe_array[i][j].partial_sum_in(partial_sum_s[i-1][j]);
-                    pe_array[i][j].activation_out(0);
-                    pe_array[i][j].partial_sum_out(partial_sum_s[i][j]);
+                    pe_grid[i][j].activation_in(activation_s[i][j-1]);
+                    pe_grid[i][j].partial_sum_in(partial_sum_s[i-1][j]);
+                    pe_grid[i][j].activation_out(activation_out[i]);
+                    pe_grid[i][j].partial_sum_out(partial_sum_s[i][j]);
                 } else {
-                    pe_array[i][j].activation_in(activation_s[i][j-1]);
-                    pe_array[i][j].partial_sum_in(partial_sum_s[i-1][j]);
-                    pe_array[i][j].activation_out(activation_s[i][j]);
-                    pe_array[i][j].partial_sum_out(partial_sum_s[i][j]);
+                    pe_grid[i][j].activation_in(activation_s[i][j-1]);
+                    pe_grid[i][j].partial_sum_in(partial_sum_s[i-1][j]);
+                    pe_grid[i][j].activation_out(activation_s[i][j]);
+                    pe_grid[i][j].partial_sum_out(partial_sum_s[i][j]);
                 }
             }
         }
@@ -127,16 +128,17 @@ private:
         }
     }
 
-    // Create the PE array
-    void create_pe_array() {
+    // Create the PE grid
+    void create_pe_grid() {
         char name[16];
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < N; ++j) {
                 snprintf(name, 16, "PE_%d_%d", i, j);
-                pe_array[i][j] = pe(name, weights[i][j]);
+                pe_grid[i][j] = pe(name, weights[i][j]);
             }
         }
     }
+
 };
 
 #endif //SYSTOLIC_ARRAY_H
