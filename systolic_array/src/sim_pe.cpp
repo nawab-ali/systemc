fd59a63f9fb17f425aad594fa316ad30912097cf
Nawab Ali <nawabali@Nawabs-MacBook-Pro.local>
2022-01-16 06:42:04 -0800

    Make weight a parameter to the simulation.

--- systolic_array/src/sim_pe.cpp
+++ systolic_array/src/sim_pe.cpp
@@ -24,7 +24,7 @@ sc_trace_file* create_vcd_trace(const char* file, sc_clock& clk, sc_signal<sc_in
     return fp;
 }
 
-void simulate_pe() {
+void simulate_pe(const sc_int<8> weight) {
     sc_trace_file* fp;
     sc_clock clk("clk", 10, SC_NS, 0.5, 1, SC_NS);
 
@@ -33,14 +33,14 @@ void simulate_pe() {
     sc_signal<sc_int<8>> activation_out;
     sc_signal<sc_int<32>> partial_sum_out;
 
-    pe pe0("PE0", 4);
+    pe pe0("PE0", weight);
     pe0.clk(clk);
     pe0.activation_in(activation_in);
     pe0.partial_sum_in(partial_sum_in);
     pe0.activation_out(activation_out);
     pe0.partial_sum_out(partial_sum_out);
 
-    pe_tb<100> pe_tb0("PE_TB0", 4);
+    pe_tb<100> pe_tb0("PE_TB0", weight);
     pe_tb0.clk(clk);
     pe_tb0.activation_out(activation_in);
     pe_tb0.partial_sum_out(partial_sum_in);
@@ -53,7 +53,9 @@ void simulate_pe() {
 }
 
 int sc_main(int argc, char** argv) {
+    const sc_int<8> weight = 4;
+
     srand(time(nullptr));
-    simulate_pe();
+    simulate_pe(weight);
     return 0;
 }
