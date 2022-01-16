a8ebfdcb02457cbbb7fa8c6317ec6a5238e943fe
Nawab Ali <nawabali@Nawabs-MacBook-Pro.local>
2022-01-16 15:08:32 -0800

    Skeleton implementation of systolic array test bench.

--- systolic_array/include/systolic_array_tb.h
+++ systolic_array/include/systolic_array_tb.h
@@ -1,16 +1,30 @@
 /**
  * @file systolic_array_tb.h
- * @brief This file implements the testbench for the Systolic Array.
+ * @brief This file implements the test bench for the Systolic Array.
  * @author Nawab Ali
  */
 
 #ifndef SYSTOLIC_ARRAY_TB_H
 #define SYSTOLIC_ARRAY_TB_H
 
+#include "util.h"
 #include <systemc.h>
 
 SC_MODULE (systolic_array_tb) {
+public:
+    sc_in<bool> clk;
+
     SC_CTOR (systolic_array_tb) {
+        SC_THREAD(gen_stimuli);
+        dont_initialize();
+        sensitive << clk.pos();
+    }
+
+private:
+    // Generate stimuli for Systolic Array
+    void gen_stimuli() {
+        wait();
+        sc_stop();
     }
 };
 
