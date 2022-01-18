cc9bc6c120c9a8669ff29d35ce9d6f8d3582b7d3
Nawab Ali <nawabali@Nawabs-MacBook-Pro.local>
2022-01-18 14:04:50 -0800

    Simulate systolic array.

--- systolic_array/src/sim_systolic_array.cpp
+++ systolic_array/src/sim_systolic_array.cpp
@@ -19,8 +19,25 @@ sc_trace_file* create_vcd_trace(const char* file, sc_clock& clk) {
 }
 
 void simulate_systolic_array() {
+    const uint8_t N = 3;
     sc_trace_file* fp;
     sc_clock clk("clk", 10, SC_NS, 0.5, 1, SC_NS);
+
+    sc_vector<sc_signal<sc_int<8>>> activation_in{"activation_in", N};
+    sc_vector<sc_signal<sc_int<32>>> partial_sum_in{"partial_sum_in", N};
+    sc_vector<sc_signal<sc_int<32>>> partial_sum_out{"partial_sum_out", N};
+
+    systolic_array<N> sa("systolic_array");
+    sa.clk(clk);
+    sa.activation_in(activation_in);
+    sa.partial_sum_in(partial_sum_in);
+    sa.partial_sum_out(partial_sum_out);
+
+    systolic_array_tb<N> sa_tb("systolic_array_tb");
+    sa_tb.clk(clk);
+    sa_tb.activation_in(activation_in);
+    sa_tb.partial_sum_in(partial_sum_in);
+    sa_tb.partial_sum_out(partial_sum_out);
 
     fp = create_vcd_trace("systolic_array", clk);
     sc_start(200, SC_NS);
